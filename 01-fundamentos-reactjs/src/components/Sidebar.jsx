476cd30456f4bfd1d42f91a8aabacf86e488f156
Pedro Germano <pedrogermano232@gmail.com>
2022-06-13 15:05:43 -0300

    Profile

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -8,13 +10,15 @@ export function Sidebar(){
           src='https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40'
           alt=""
         />
-        <div className='styles.profile'>
+        <div className={styles.profile}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/7785179?v=4" />
           <strong>Pedro Germano</strong>
           <span>Frontend</span>
         </div>
 
         <footer>
           <a href="#">
+            <PencilSimpleLine  size={20}/>
             Editar seu perfil
           </a>
         </footer>
