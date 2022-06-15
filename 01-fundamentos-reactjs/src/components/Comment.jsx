ab1d7beeb48fbf5935f76938d08e009b052066a6
Pedro Germano <pedrogermano232@gmail.com>
2022-06-15 15:06:57 -0300

    Component Comment done

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/pedroGermano.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Germano</strong>
+              <time title="13 de junho às 08:13h" dateTime="2022-06-13 08:13:30">cerca de 2h</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
